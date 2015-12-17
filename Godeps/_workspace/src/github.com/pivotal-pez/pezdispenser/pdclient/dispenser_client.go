390c02ad42d26474a82d8015e12784a76aec9db2
John Calabrese <jcalabrese@pivotal.io>
2015-12-16 21:54:16 -0500

    [#110117032] vendoring in version with better log output

--- Godeps/_workspace/src/github.com/pivotal-pez/pezdispenser/pdclient/dispenser_client.go
+++ Godeps/_workspace/src/github.com/pivotal-pez/pezdispenser/pdclient/dispenser_client.go
@@ -31,6 +31,8 @@ func (s *PDClient) PostLease(leaseID, inventoryID, skuID string, leaseDaysDurati
 			json.Unmarshal(resBodyBytes, &leaseCreateResponse)
 
 		} else {
+			lo.G.Error("client Do Error: ", err.Error())
+			lo.G.Error("client Res: ", res)
 			err = ErrInvalidDispenserResponse
 		}
 
