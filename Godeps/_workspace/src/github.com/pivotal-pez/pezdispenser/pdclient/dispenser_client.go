ebea89c26841b73849737f66873c14732c3a778d
John Calabrese <jcalabrese@pivotal.io>
2015-12-16 21:46:47 -0500

    [#110117032] vendoring in bugfix

--- Godeps/_workspace/src/github.com/pivotal-pez/pezdispenser/pdclient/dispenser_client.go
+++ Godeps/_workspace/src/github.com/pivotal-pez/pezdispenser/pdclient/dispenser_client.go
@@ -25,13 +25,8 @@ func (s *PDClient) PostLease(leaseID, inventoryID, skuID string, leaseDaysDurati
 	var body io.Reader
 	if body, err = s.getRequestBody(leaseID, inventoryID, skuID, leaseDaysDuration); err == nil {
 		req, _ := s.createRequest("POST", fmt.Sprintf("%s/v1/lease", s.URL), body)
-		lo.G.Debug("req: ", req)
-		lo.G.Debug("client: ", s.client)
-		res, err = s.client.Do(req)
-		lo.G.Debug("res: ", res)
-		lo.G.Debug("err: ", err)
 
-		if err == nil && res.StatusCode == http.StatusOK {
+		if res, err = s.client.Do(req); err == nil && res.StatusCode == http.StatusOK {
 			resBodyBytes, _ := ioutil.ReadAll(res.Body)
 			json.Unmarshal(resBodyBytes, &leaseCreateResponse)
 
